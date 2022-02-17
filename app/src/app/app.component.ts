769cda656845940787d1a20ca71828e232fbac77
amir ghaderian <amir@amirs-MacBook-Air.local>
2022-02-17 19:19:20 +0330

    set showStudentById()

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -12,37 +12,32 @@ export class AppComponent {
   detailFirst = "amaroooo"
 
 
-  showingStudent = { firstName : '',
-                    lastName : '',
-                  };
+  showingStudent = {
+    firstName: '',
+    lastName: '',
+  };
 
 
   students: Array<any> = [
-            { firstName: "ali", lastName: "ghaderian", age: 34, dateBirth: "1987", code: "0032244455", job: "designer", lesson: [1, 24], id: 11, picProfile: "ASDF.JPG" },
-            { firstName: "amir", lastName: "ghaderian", age: 35, dateBirth: "1986", code: "0081125674", job: "front", lesson: [5, 24, 1, 3, 66], id: 4, picProfile: "1.jpeg" },
-            { firstName: "aida", lastName: "amiri", age: 20, dateBirth: "2001", code: "0032244455", job: "Airline employee", lesson: [1, 5, 66], id: 31, picProfile: "2.jpeg" },
-            { firstName: "houman", lastName: "ghaderian", age: 42, dateBirth: "1977", code: "0032244455", job: "CEO", lesson: [5, 46, 3], id: 5, picProfile: "ASD.PNG" },
-            { firstName: "hani", lastName: "ghaderian", age: 47, dateBirth: "1977", code: "32488488493", job: "teacher", lesson: [5], id: 22, picProfile: "3.jpeg" }];
-        
+    { firstName: "ali", lastName: "ghaderian", age: 34, dateBirth: "1987", code: "0032244455", job: "designer", lesson: [1, 24], id: 11, picProfile: "ASDF.JPG" },
+    { firstName: "amir", lastName: "ghaderian", age: 35, dateBirth: "1986", code: "0081125674", job: "front", lesson: [5, 24, 1, 3, 66], id: 4, picProfile: "1.jpeg" },
+    { firstName: "aida", lastName: "amiri", age: 20, dateBirth: "2001", code: "0032244455", job: "Airline employee", lesson: [1, 5, 66], id: 31, picProfile: "2.jpeg" },
+    { firstName: "houman", lastName: "ghaderian", age: 42, dateBirth: "1977", code: "0032244455", job: "CEO", lesson: [5, 46, 3], id: 5, picProfile: "ASD.PNG" },
+    { firstName: "hani", lastName: "ghaderian", age: 47, dateBirth: "1977", code: "32488488493", job: "teacher", lesson: [5], id: 22, picProfile: "3.jpeg" }];
 
 
-  showSecond() {
-    this.showingStudent = this.students[0];
-  }
 
-
-  showFirst() {
-    this.showingStudent = this.students[1];
-  }
-
-  showThird() {
-    this.showStudentById(22)
-  }
 
 
   showStudentById(id: number) {
-
-    // this.showingStudent = student by id
+   
+    for (var i = 0; i < this.students.length; i++){
+      if(this.students[i].id===id){
+        this.showingStudent = this.students[i];
+       
+      }
+    }
+  
 
   }
 }
