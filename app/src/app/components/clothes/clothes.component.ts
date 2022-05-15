4799be48548602f3b96d934eb3a3718fd9388ae4
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-05-15 22:11:41 +0430

    currentIndex phat

--- app/src/app/components/clothes/clothes.component.ts
+++ app/src/app/components/clothes/clothes.component.ts
@@ -22,14 +22,14 @@ export class ClothesComponent implements OnInit {
     { title: 1123, images: ["assets/pic/13.jpg", "assets/pic/1.jpg"] }
   ]
    currentImge:string[]= [];
- 
+   currentIndex:number[]=[];
+   
   constructor() { 
     for(var i=0;i<this.clothes.length;i++){
       this.currentImge.push(this.clothes[i].images[0]);
+      this.currentIndex.push(0)
+    }
     
-
-     
-  }
  
   }
   
@@ -38,12 +38,13 @@ export class ClothesComponent implements OnInit {
   
   
   showingImage(id:number,clothesNum:number) {
+
     this.currentImge[clothesNum]=this.clothes[clothesNum].images[id];
-    
+    this.currentIndex[clothesNum]=id+1;
     
   
    
-    console.log( this.clothes[clothesNum].images[id])
+    console.log(this.currentIndex)
     
 
 
