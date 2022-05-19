27836f58472df68719a946b28351edbfbd08150e
amir ghaderian <79098240+amir-ghaderian@users.noreply.github.com>
2022-05-19 21:53:13 +0430

    set next and previous clothes

--- app/src/app/components/clothes/clothes.component.ts
+++ app/src/app/components/clothes/clothes.component.ts
@@ -7,23 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClothesComponent implements OnInit {
   clothes = [
-    { title: 1110, images: ["assets/pic/1.jpg", "assets/pic/13.jpg"] },
-    { title: 1111, images: ["assets/pic/2.jpg", "assets/pic/12.jpg"] },
-    { title: 1112, images: ["assets/pic/3.jpg", "assets/pic/11.jpg"] },
-    { title: 1113, images: ["assets/pic/4.jpg", "assets/pic/10.jpg"] },
-    { title: 1114, images: ["assets/pic/5.jpg", "assets/pic/9.jpg", "assets/pic/5.jpg", "assets/pic/6.jpg"] },
-    { title: 1115, images: ["assets/pic/6.jpg", "assets/pic/8.jpg"] },
-    { title: 1116, images: ["assets/pic/7.jpg", "assets/pic/3.jpg"] },
-    { title: 1117, images: ["assets/pic/8.jpg"] },
-    { title: 1118, images: ["assets/pic/9.jpg", "assets/pic/5.jpg"] },
-    { title: 1119, images: ["assets/pic/10.jpg", "assets/pic/4.jpg"] },
-    { title: 1121, images: ["assets/pic/11.jpg", "assets/pic/3.jpg"] },
-    { title: 1122, images: ["assets/pic/12.jpg", "assets/pic/2.jpg"] },
-    { title: 1123, images: ["assets/pic/13.jpg", "assets/pic/1.jpg"] }
+    { title: 1110, images: ["assets/pic/1.jpg", "assets/pic/13.jpg"]  ,click:0},
+    { title: 1111, images: ["assets/pic/2.jpg", "assets/pic/12.jpg"]  ,click:0},
+    { title: 1112, images: ["assets/pic/3.jpg", "assets/pic/11.jpg"]  ,click:0},
+    { title: 1113, images: ["assets/pic/4.jpg", "assets/pic/10.jpg"]  ,click:0},
+    { title: 1114, images: ["assets/pic/5.jpg", "assets/pic/9.jpg", "assets/pic/5.jpg", "assets/pic/6.jpg"]  ,click:0},
+    { title: 1115, images: ["assets/pic/6.jpg", "assets/pic/8.jpg"]  ,click:0},
+    { title: 1116, images: ["assets/pic/7.jpg", "assets/pic/3.jpg"]  ,click:0},
+    { title: 1117, images: ["assets/pic/8.jpg"]  ,click:0},
+    { title: 1118, images: ["assets/pic/9.jpg", "assets/pic/5.jpg"]  ,click:0},
+    { title: 1119, images: ["assets/pic/10.jpg", "assets/pic/4.jpg"]  ,click:0},
+    { title: 1121, images: ["assets/pic/11.jpg", "assets/pic/3.jpg"]  ,click:0},
+    { title: 1122, images: ["assets/pic/12.jpg", "assets/pic/2.jpg"]  ,click:0},
+    { title: 1123, images: ["assets/pic/13.jpg", "assets/pic/1.jpg"]  ,click:0}
   ]
   currentImge: string[] = [];
   currentIndex: number[] = [];
-  click = 0;
+
 
   constructor() {
     for (var i = 0; i < this.clothes.length; i++) {
@@ -47,20 +47,22 @@ export class ClothesComponent implements OnInit {
 
 
 
-  previousClothes(id: number, items: number) {
-    // this.click += 1
-    // this.currentImge[items] = this.clothes[items].images[this.click];
-    // this.currentIndex[items] = id;
-    // console.log(this.clothes[items].images)
-    // console.log(this.clothes[items].images[id])
+  previousClothes(id: number, clothesNum: number) {
+    this.clothes[clothesNum].click-=1;
+    
+     this.currentImge[clothesNum] = this.clothes[clothesNum].images[this.clothes[clothesNum].click];
+     this.currentIndex[clothesNum] = this.clothes[clothesNum].click;
+console.log(this.clothes[clothesNum].click)
     
   }
 
 
   nextClothes(id: number, clothesNum: number) {
-    this.click += 1
-    this.currentImge[clothesNum] = this.clothes[clothesNum].images[this.click];
-    this.currentIndex[clothesNum] = this.click;
+
+  
+    this.clothes[clothesNum].click+=1
+    this.currentImge[clothesNum] = this.clothes[clothesNum].images[this.clothes[clothesNum].click];
+    this.currentIndex[clothesNum] = this.clothes[clothesNum].click;
    
 
    
